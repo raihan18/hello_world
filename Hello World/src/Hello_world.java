d296449b34c57675ad2f0921c450a30c2a2b353a
raihan18 <raihan.nayeem@gmail.com>
2014-07-07 14:00:39 +0600

    fast commit

---
+++ Hello World/src/Hello_world.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author nayeem
+ *
+ */
+public class Hello_world {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
